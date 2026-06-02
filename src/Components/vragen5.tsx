f940eef27f32d33d74cf34e08e6ea242d8715ba7
meijerjeremy02-hub <meijerjeremy02@gmail.com>
2026-06-02 14:24:33 +0200

    Vragen v3

--- src/Components/vragen5.tsx
+++ src/Components/vragen5.tsx
@@ -3,14 +3,28 @@ const borderKleur = ['border-blue-400', 'border-blue-500', 'border-blue-600', 'b
 function VraagRij({ tekst }: { tekst: string }) {
   const [waarde, setWaarde] = useState<number | null>(null)
   return (
-    <div className="border border-purple-950 p-4 text-center">
+    <div className="border border-blue-950 p-4 text-center">
       <span className="text-md">{tekst}</span>
       <div className="flex gap-3 mt-2">
-        <div className="flex-1"><h1 className="text-purple-400">oneens</h1></div>
+        <div className="flex-1">
+          <h1 className="text-blue-400">
+            oneens
+          </h1>
+        </div>
         {[1, 2, 3, 4, 5].map((n, i) => (
-          <button key={n} onClick={() => setWaarde(n)} className={`flex-2 w-8 h-8 rounded-full border-2 ${borderKleur[i]} text-xs font-medium transition-colors cursor-pointer ${waarde === n ? 'bg-orange-400 text-white' : ''}`}>{n}</button>
+          <button
+            key={n}
+            onClick={() => setWaarde(n)}
+            className={`flex-2 w-8 h-8 rounded-full border-2 ${borderKleur[i]} text-xs font-medium transition-colors cursor-pointer ${waarde === n ? 'bg-orange-400 text-white' : ''}`}
+          >
+            {n}
+          </button>
         ))}
-        <div className="flex-1"><h1 className="text-purple-900">eens</h1></div>
+        <div className="flex-1">
+          <h1 className="text-blue-900">
+            eens
+          </h1>
+        </div>
       </div>
     </div>
   )
