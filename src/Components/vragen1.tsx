c5c533900cacfdb8679669c1c97b1425ade62a3b
meijerjeremy02-hub <meijerjeremy02@gmail.com>
2026-06-17 13:47:37 +0200

    fix phone

--- src/Components/vragen1.tsx
+++ src/Components/vragen1.tsx
@@ -3,28 +3,28 @@ const borderKleur = ['border-blue-400', 'border-blue-500', 'border-blue-600', 'b
 function VraagRij({ tekst }: { tekst: string }) {
   const [waarde, setWaarde] = useState<number | null>(null)
   return (
-    <div className="border border-blue-950 p-4 text-center">
+    <div className="border border-blue-950 p-3 text-center">
       <span className="text-md">{tekst}</span>
-      <div className="flex gap-3 mt-2">
-        <div className="flex-1">
-          <h1 className="text-blue-400">
-            oneens
-          </h1>
+      <div className="flex items-center gap-1 sm:gap-3 mt-2">
+        <h1 className="shrink-0 w-12 sm:w-16 text-blue-400 text-xs sm:text-base text-left">
+          oneens
+        </h1>
+
+        <div className="flex flex-1 justify-between text-center gap-1 sm:gap-3 md:mx-auto">
+          {[1, 2, 3, 4, 5].map((n, i) => (
+            <button
+              key={n}
+              onClick={() => setWaarde(n)}
+              className={`shrink-0 w-[20%] h-7 sm:w-1/6 sm:h-8 rounded-full border-2 ${borderKleur[i]} text-xs font-medium transition-colors cursor-pointer ${waarde === n ? 'bg-orange-400 text-white' : ''}`}
+            >
+              {n}
+            </button>
+          ))}
         </div>
-        {[1, 2, 3, 4, 5].map((n, i) => (
-          <button
-            key={n}
-            onClick={() => setWaarde(n)}
-            className={`flex-2 w-8 h-8 rounded-full border-2 ${borderKleur[i]} text-xs font-medium transition-colors cursor-pointer ${waarde === n ? 'bg-orange-400 text-white' : ''}`}
-          >
-            {n}
-          </button>
-        ))}
-        <div className="flex-1">
-          <h1 className="text-blue-900">
-            eens
-          </h1>
-        </div>
+
+        <h1 className="shrink-0 w-12 text-right sm:w-16 text-blue-900 text-xs sm:text-base md:mx-auto">
+          eens
+        </h1>
       </div>
     </div>
   )
