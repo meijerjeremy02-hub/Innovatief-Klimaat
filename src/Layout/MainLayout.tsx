a4c79d96766c93c1957e3cd2d6b123109b5e9f41
meijerjeremy02-hub <meijerjeremy02@gmail.com>
2026-08-11 23:06:16 +0200

    undone fix

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,17 +1,8 @@
 import { Outlet } from 'react-router'
 import Footer from '../Components/Footer'
 import Orange_Balk from '../Components/Oranje_Balk'
-import { useEffect } from 'react'
-import { useLocation } from 'react-router'
 
 export default function MainLayout() {
-
-  const {pathname} = useLocation();
-
-  useEffect(() => {
-    document.getElementById('top')?.scrollIntoView({ behavior: 'instant' })
-  }, [pathname]);
-
   return (
     <main className="min-h-dvh bg-linear-to-b from-orange-400 via-[#DECAB7] to-blue-900 overflow-x-hidden">
       <div className="anchor top-0" id="top"></div>
